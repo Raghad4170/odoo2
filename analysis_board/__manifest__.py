9c6f7829c3a78990a1096e1984dd360024b0b94c
Raghad4170 <94894817+Raghad4170@users.noreply.github.com>
2022-04-02 11:29:42 +0300

    Add files via upload

---
+++ analysis_board/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright to Mutn
+{
+    'name': 'analysis board',
+    'version': '2.0',
+    'category': 'Human Resources/Attendances',
+    'sequence': 1,
+    'summary': '',
+    'description': """""",
+    'author': "Mutn",
+    'website': "www.mutn.tech",
+    'license': 'LGPL-3',
+    'depends': ['base','employeesinfo','board'],
+    'data': [
+        'menu.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'application': True,
+}
